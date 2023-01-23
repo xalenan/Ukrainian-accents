f1c4685e2637f9268dacc57c4dd7b7d8e92049f0
Elena Nazarova <93036971+xalenan@users.noreply.github.com>
2023-01-23 23:55:27 +0200

    Update accents.js

--- script/accents.js
+++ script/accents.js
@@ -1,40 +1,13 @@
 const getWordsAccents = () => {
     const links = document.querySelectorAll('.abc__link');  
     console.log(links);
-//     const renderAccents = (accents) => {
-//         const accentsContainer = document.querySelector('.info__wrapper');  
-
-//         accentsContainer.innerHTML = '';
-
-//         accents.forEach(accent => {
-//             const accentBlock = document.createElement('div');
-//             accentBlock.classList.add('.info__items');
-
-//             accentBlock.innerHTML = `
-//                 <p class="info__word">${accent.accent}</p>
-//             `
-
-//             accentsContainer.append(accentBlock);
-//         })
-
-//     }
 
     const getData = () => {
         fetch('https://accents-dba57-default-rtdb.firebaseio.com/db.json')
             .then((res) => res.json())
             .then((data) => {
                 console.log(data);
-//                 const array = data.filter((item) => item.id === value);
-
-//                 localStorage.setItem('accents', JSON.stringify(array));
-
-//                 if (window.location.pathname !== '/accents.html') {
-//                     window.location.href = 'accents.html';
-//                 } else {
-//                     renderAccents(array)
-//                 }
             }) 
-
     }
 
     links.forEach((link) => {
@@ -42,43 +15,24 @@ const getWordsAccents = () => {
             event.preventDefault();
                 console.log("hello");
             getData();
-//             const linkValue = link.textContent;
 
-//             getData(linkValue);
         })
     })
-
-//     if (localStorage.getItem('accents') && window.location.pathname === '/accents.html') {
-//         renderAccents(JSON.parse(localStorage.getItem('accents')))
-//     }
-
-//         // Class Active
-//     const linksWrapper = document.querySelector('.abc__wrapper');
-
-//     function changeClassActive() {
-//         linksWrapper.addEventListener('click', (event) => {
-//             links.forEach(links => links.classList.remove('active'));
-//             event.target.classList.add('active'); 
-//         })
-//     }
-
-//     changeClassActive()
-    
 }
 
 getWordsAccents();
 
 
-// const getAccents = () => {
-//     fetch('https://accents-dba57-default-rtdb.firebaseio.com/db.json')
-//     .then((res) => res.json())
-//         .then((data) => {
-//             console.log(data);
+const getAccents = () => {
+    fetch('https://accents-dba57-default-rtdb.firebaseio.com/db.json')
+    .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
             
-//         }) 
-// }
+        }) 
+}
 
-// getAccents();
+getAccents();
 
 
 
