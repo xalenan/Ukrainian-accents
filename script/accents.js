245ba0c909fc26bdd5267088115a011f0b9a5a77
Elena Nazarova <93036971+xalenan@users.noreply.github.com>
2023-01-24 13:42:10 +0200

    Update accents.js

--- script/accents.js
+++ script/accents.js
@@ -1,6 +1,24 @@
 const getWordsAccents = () => {
     const links = document.querySelectorAll('.abc__link');  
     
+    const renderAccents = (accents) => {
+        const accentsContainer = document.querySelector('.info__wrapper');  
+
+        accentsContainer.innerHTML = '';
+
+        accents.forEach(accent => {
+            const accentBlock = document.createElement('div');
+            accentBlock.classList.add('.info__items');
+
+            accentBlock.innerHTML = `
+                <p class="info__word">${accent.accent}</p>
+            `
+
+            accentsContainer.append(accentBlock);
+        })
+
+    }
+        
     const getData = (value) => {
         fetch('https://accents-dba57-default-rtdb.firebaseio.com/db.json')
             .then((res) => res.json())
@@ -9,10 +27,12 @@ const getWordsAccents = () => {
                 const array = data.filter((item) => item.id === value);
             
                 localStorage.setItem('accents', JSON.stringify(array));
-//                 window.location.href = 'accents.html';
+
                 console.log(window.location);
                 if (window.location.pathname !== "/Ukrainian-accents/accents.html") {
                     window.location.href = 'accents.html';
+                } else {
+                    renderAccents(array);
                 }
             }) 
     }
@@ -28,6 +48,20 @@ const getWordsAccents = () => {
 
         })
     })
+    
+    if (localStorage.getItem('accents') && window.location.pathname === "/Ukrainian-accents/accents.html") {
+        renderAccents(JSON.parse(localStorage.getItem('accents')))
+    }
+    
+     // Class Active
+    const linksWrapper = document.querySelector('.abc__wrapper');
+
+    function changeClassActive() {
+        linksWrapper.addEventListener('click', (event) => {
+            links.forEach(links => links.classList.remove('active'));
+            event.target.classList.add('active'); 
+        })
+    }
 }
 
 getWordsAccents();
